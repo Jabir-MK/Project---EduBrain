d64428829708df11b774d91daba8e873f18d2c96
Jabir MK <jabirmanakkanakath@gmail.com>
2023-03-02 15:09:02 +0530

    Minor Fixes

--- lib/teacher/contents_screens/grades/add_grades/add_grade_screen.dart
+++ lib/teacher/contents_screens/grades/add_grades/add_grade_screen.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'dart:developer';
-
 import 'package:edubrain/constants/constant.dart';
 import 'package:edubrain/database/functions/student_section.dart';
 import 'package:edubrain/database/model/grades/grades_model.dart';
