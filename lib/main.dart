fecb3b0d26a034d32a25955e04e9c4b657aaacef
Jabir MK <jabirmanakkanakath@gmail.com>
2023-03-03 10:05:18 +0530

    Started State Management Conversion

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:edubrain/constants/constant.dart';
 import 'package:edubrain/constants/fontstyle_constants.dart';
 import 'package:edubrain/constants/routes.dart';
 import 'package:edubrain/comon_screens/splash_screen/splash_screen.dart';
+import 'package:edubrain/controller/teacher/teacher_login_controller.dart';
 import 'package:edubrain/database/model/assignment/assignment_data_model.dart';
 import 'package:edubrain/database/model/grades/grades_model.dart';
 import 'package:edubrain/database/model/student/student_data_model.dart';
 import 'package:edubrain/database/model/teacher_model/teacher_data_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 
 bool userTeacherLoginKey = false;
 bool userStudentLoginKey = false;
@@ -39,66 +41,71 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "EduBrain",
-      theme: ThemeData.light().copyWith(
-        scaffoldBackgroundColor: jPrimaryColor,
-        primaryColor: jPrimaryColor,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: jAppBarBackgroundColor,
-          titleTextStyle: jAkayaTelivigalaAppDefaultStyle,
-          centerTitle: true,
-          elevation: 15,
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          labelStyle: TextStyle(
-            fontSize: 20,
-            color: jPrimaryColor,
-            height: 0.5,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TeacherProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "EduBrain",
+        theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: jPrimaryColor,
+          primaryColor: jPrimaryColor,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: jAppBarBackgroundColor,
+            titleTextStyle: jAkayaTelivigalaAppDefaultStyle,
+            centerTitle: true,
+            elevation: 15,
           ),
-          hintStyle: TextStyle(
-            fontSize: 16,
-            color: jBlackTextColor,
-            height: 0.5,
-          ),
-          enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: jLightTextColor,
-              width: 0.7,
-            ),
-          ),
-          border: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: jLightTextColor,
-            ),
-          ),
-          disabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: jLightTextColor,
-            ),
-          ),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
+          inputDecorationTheme: const InputDecorationTheme(
+            labelStyle: TextStyle(
+              fontSize: 20,
               color: jPrimaryColor,
+              height: 0.5,
             ),
-          ),
-          errorBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: jErrorBorderColor,
-              width: 1.2,
+            hintStyle: TextStyle(
+              fontSize: 16,
+              color: jBlackTextColor,
+              height: 0.5,
             ),
-          ),
-          focusedErrorBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: jErrorBorderColor,
-              width: 1.2,
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: jLightTextColor,
+                width: 0.7,
+              ),
+            ),
+            border: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: jLightTextColor,
+              ),
+            ),
+            disabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: jLightTextColor,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: jPrimaryColor,
+              ),
+            ),
+            errorBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: jErrorBorderColor,
+                width: 1.2,
+              ),
+            ),
+            focusedErrorBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: jErrorBorderColor,
+                width: 1.2,
+              ),
             ),
           ),
         ),
+        initialRoute: SplashScreen.routeName,
+        routes: routes,
       ),
-      initialRoute: SplashScreen.routeName,
-      routes: routes,
     );
   }
 }
--- lib/view/teacher/login_screen/login_teacher/teacher_login_screen.dart
+++ lib/view/teacher/login_screen/login_teacher/teacher_login_screen.dart
@@ -1,41 +1,22 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:edubrain/constants/constant.dart';
-import 'package:edubrain/database/functions/teacher_section.dart';
+import 'package:edubrain/controller/teacher/teacher_login_controller.dart';
 import 'package:edubrain/database/model/teacher_model/teacher_data_model.dart';
-import 'package:edubrain/main.dart';
-import 'package:edubrain/view/teacher/home_screen/teacher_home_screen.dart';
 import 'package:edubrain/view/teacher/login_screen/signup/signup_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
 
 TeacherModel? userTeacher;
 
-class TeacherLoginScreen extends StatefulWidget {
+class TeacherLoginScreen extends StatelessWidget {
   const TeacherLoginScreen({super.key});
 
   static String routeName = 'TeacherLoginScreen';
 
   @override
-  State<TeacherLoginScreen> createState() => _TeacherLoginScreenState();
-}
-
-class _TeacherLoginScreenState extends State<TeacherLoginScreen> {
-  final TextEditingController _teacherLoginDataController =
-      TextEditingController();
-
-  final TextEditingController _teacherLoginPassController =
-      TextEditingController();
-
-  final _formKey = GlobalKey<FormState>();
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<TeacherProvider>(context, listen: false);
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
@@ -77,7 +58,6 @@ class _TeacherLoginScreenState extends State<TeacherLoginScreen> {
             ),
             Container(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
               decoration: const BoxDecoration(
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(jDefaultPadding * 3),
@@ -91,14 +71,15 @@ class _TeacherLoginScreenState extends State<TeacherLoginScreen> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Form(
-                      key: _formKey,
+                      key: provider.formKey,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
+                          provider.buildEmailField(
+                              provider.teacherLoginDataController),
                           jheightBox,
-                          buildEmailField(),
-                          jheightBox,
-                          buildPasswordField(),
+                          provider.buildPasswordField(
+                              provider.teacherLoginPassController),
                           jheightBox,
                           jheightBox,
                           Row(
@@ -113,8 +94,13 @@ class _TeacherLoginScreenState extends State<TeacherLoginScreen> {
                                         jPrimaryColor),
                                   ),
                                   onPressed: () async {
-                                    if (_formKey.currentState!.validate()) {
-                                      checkTeacherLogin();
+                                    if (provider.formKey.currentState!
+                                        .validate()) {
+                                      provider.checkTeacherLogin(
+                                        context,
+                                        provider.teacherLoginDataController,
+                                        provider.teacherLoginPassController,
+                                      );
                                     }
                                   },
                                   icon: const Icon(Icons.check),
@@ -159,91 +145,5 @@ class _TeacherLoginScreenState extends State<TeacherLoginScreen> {
         ),
       ),
     );
-  }
-
-  TextFormField buildEmailField() {
-    return TextFormField(
-      controller: _teacherLoginDataController,
-      textAlign: TextAlign.start,
-      keyboardType: TextInputType.emailAddress,
-      style: const TextStyle(
-        color: jBlackTextColor,
-        fontWeight: FontWeight.w600,
-        fontSize: 17,
-      ),
-      decoration: const InputDecoration(
-        labelText: "E-mail ",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        isDense: true,
-      ),
-      validator: (value) {
-        RegExp regExp = RegExp(emailPattern);
-        if (value == null || value.isEmpty) {
-          return "Please Enter Your E-mail";
-        } else if (!regExp.hasMatch(value)) {
-          return "Please enter valid Email";
-        } else {
-          return null;
-        }
-      },
-    );
-  }
-
-  TextFormField buildPasswordField() {
-    return TextFormField(
-      controller: _teacherLoginPassController,
-      textAlign: TextAlign.start,
-      obscureText: true,
-      keyboardType: TextInputType.name,
-      style: const TextStyle(
-        color: jBlackTextColor,
-        fontWeight: FontWeight.w600,
-        fontSize: 17,
-      ),
-      decoration: const InputDecoration(
-        labelText: "Password",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        isDense: true,
-      ),
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return "Password not filled";
-        } else if (value.length < 8) {
-          return 'Password must be atleast 8 characters';
-        } else {
-          return null;
-        }
-      },
-    );
-  }
-
-  Future<void> checkTeacherLogin() async {
-    final teacherLoginDataCheck = _teacherLoginDataController.text.trim();
-    final teacherLoginPasswordCheck = _teacherLoginPassController.text.trim();
-
-    final shredpref = await SharedPreferences.getInstance();
-    await shredpref.setString('techerName', teacherLoginDataCheck);
-    await shredpref.setString('techerPass', teacherLoginPasswordCheck);
-
-    Box<TeacherModel> profileCheckLoginTeacher =
-        await Hive.openBox<TeacherModel>(teacherModelDatabaseName);
-    profileCheckLoginTeacher.values
-        .where((element) =>
-            element.teacherEMail == teacherLoginDataCheck &&
-            element.teacherPassword == teacherLoginPasswordCheck)
-        .forEach(
-      (element) {
-        userTeacher = element;
-        loginCheckTeacher(context);
-      },
-    );
-  }
-
-  void loginCheckTeacher(BuildContext context) async {
-    final sharedPrefTeacher = await SharedPreferences.getInstance();
-    await sharedPrefTeacher.setBool(teacherSaveKey, true);
-    Navigator.of(context).pushReplacement(MaterialPageRoute(
-      builder: (context) => const TeacherHomeScreen(),
-    ));
   }
 }
